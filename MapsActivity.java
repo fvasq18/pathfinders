c14358abff678a950c02b65f9b739863151421a8
ethan-gimmestad <122394161+ethan-gimmestad@users.noreply.github.com>
2024-09-27 22:18:08 -0600

    Update MapsActivity.java

--- MapsActivity.java
+++ MapsActivity.java
@@ -1,6 +1,5 @@
-package unco.edu.gpstrackingdemo1;
+package unco.edu.pathfinders;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
 import android.location.Location;
@@ -17,81 +16,52 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.List;
 
-import unco.edu.gpstrackingdemo1.databinding.ActivityMapsBinding;
-
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-
     List<Location> savedLocations;
-
-
     private GoogleMap mMap;
-    private ActivityMapsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps); // Linking to the correct layout
 
-        binding = ActivityMapsBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        // Obtain the SupportMapFragment and get notified when the map is ready
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        MyApplication myApplication = (MyApplication)getApplicationContext();
+        MyApplication myApplication = (MyApplication) getApplicationContext();
         savedLocations = myApplication.getMyLocations();
-
-
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng lastLocationPlaced = null;
 
-        LatLng lastLocationPlaced = sydney;
-
-        for (Location location: savedLocations) {
+        // Add markers for saved locations
+        for (Location location : savedLocations) {
             LatLng latlng = new LatLng(location.getLatitude(), location.getLongitude());
-            MarkerOptions markerOptions = new MarkerOptions();
-            markerOptions.position(latlng);
-            markerOptions.title("Lat:" + location.getLatitude() + " Long:" + location.getLongitude());
+            MarkerOptions markerOptions = new MarkerOptions()
+                    .position(latlng)
+                    .title("Lat: " + location.getLatitude() + ", Lon: " + location.getLongitude());
             mMap.addMarker(markerOptions);
             lastLocationPlaced = latlng;
         }
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(lastLocationPlaced, 12.0f));
 
-        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(@NonNull Marker marker) {
-                //lets count the number of times pin is clicked
+        if (lastLocationPlaced != null) {
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(lastLocationPlaced, 12.0f));
+        }
 
-                Integer clicks = (Integer) marker.getTag();
-                if (clicks == null) {
-                    clicks = 0;
-                }
-                clicks++;
-                marker.setTag(clicks);
-                Toast.makeText(MapsActivity.this, "Marker " + marker.getTitle() + "was clicked" + marker.getTag() + " times." , Toast.LENGTH_SHORT).show();
-
-
+        // Handle marker click events
+        mMap.setOnMarkerClickListener(marker -> {
+            Integer clicks = (Integer) marker.getTag();
+            clicks = clicks == null ? 1 : clicks + 1;
+            marker.setTag(clicks);
+            Toast.makeText(MapsActivity.this, marker.getTitle() + " clicked " + clicks + " times", Toast.LENGTH_SHORT).show();
             return false;
-            }
         });
     }
 }
